9d15d5e6531cdc13576213af515ba97087de6159
FahmiNB <fahmi123ganers@gmail.com>
2022-08-23 20:50:57 +0700

    typo

--- src/utils/index.js
+++ src/utils/index.js
@@ -22,7 +22,7 @@ const getInitialData = () => [
   },
   {
     id: 4,
-    title: "Lifecycle",
+    title: "Lifecycl e",
     body: "Dalam konteks React component, lifecycle merupakan kumpulan method yang menjadi siklus hidup mulai dari component dibuat (constructor), dicetak (render), pasca-cetak (componentDidMount), dan sebagainya. ",
     createdAt: "2022-04-14T04:27:34.572Z",
     archived: false,
